e0c8c307371cd83d72fe4087fb5c5bd51cc7f181
Nokocchi <59347338+Nokocchi@users.noreply.github.com>
2025-11-02 14:32:41 +0100

    Simplified and sped up contract tests by removing dependency on Spring ApplicationContext and web server.

--- banking-core/src/contractTest/java/com/alph/core/customer/CustomerBase.java
+++ banking-core/src/contractTest/java/com/alph/core/customer/CustomerBase.java
@@ -1,20 +1,14 @@
 package com.alph.core.customer;
 
-import com.alph.core.ContractBase;
 import com.alphbank.commons.impl.JsonLog;
-import com.alphbank.core.account.service.repository.AccountRepository;
 import com.alphbank.core.customer.rest.CustomerController;
 import com.alphbank.core.customer.rest.model.Address;
 import com.alphbank.core.customer.rest.model.Customer;
 import com.alphbank.core.customer.service.CustomerService;
-import com.alphbank.core.customer.service.repository.AddressRepository;
-import com.alphbank.core.customer.service.repository.CustomerRepository;
-import com.alphbank.core.loan.service.repository.LoanRepository;
-import com.alphbank.core.payment.service.repository.PaymentRepository;
+import io.restassured.module.webtestclient.RestAssuredWebTestClient;
 import org.junit.jupiter.api.BeforeEach;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.mockito.Mockito;
+import org.springframework.test.context.ContextConfiguration;
 import reactor.core.publisher.Flux;
 
 import java.util.Locale;
@@ -22,31 +16,16 @@ import java.util.UUID;
 
 import static org.mockito.Mockito.when;
 
-public class CustomerBase extends ContractBase {
+@ContextConfiguration(classes = JsonLog.class)
+public class CustomerBase {
 
-    @MockitoBean
-    CustomerService customerService;
-
-    @MockitoBean
-    AddressRepository addressRepository;
-
-    @MockitoBean
-    CustomerRepository customerRepository;
-
-    @MockitoBean
-    AccountRepository accountRepository;
-
-    @MockitoBean
-    PaymentRepository paymentRepository;
-
-    @MockitoBean
-    LoanRepository loanRepository;
-
-    @Autowired
-    JsonLog jsonLog;
+    CustomerService customerService = Mockito.mock(CustomerService.class);
+    JsonLog jsonLog = Mockito.mock(JsonLog.class);
 
     @BeforeEach
-    void beforeEach(){
+    void beforeEach() {
+        RestAssuredWebTestClient.standaloneSetup(new CustomerController(customerService, jsonLog));
+
         Customer customer = new Customer(UUID.fromString("509c0b04-b1f4-4b72-bac9-3e50e9fbcee4"), new Address("street", "city", "country"), "phone", "nationalId", "firstName", "lastName", Locale.of("sv", "SE"));
         when(customerService.findAllCustomers()).thenReturn(Flux.just(customer));
     }
