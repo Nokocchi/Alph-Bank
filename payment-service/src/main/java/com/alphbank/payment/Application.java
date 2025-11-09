4355b30e77ec896da29ef5ef546f1b24561fdd61
Nokocchi <59347338+Nokocchi@users.noreply.github.com>
2025-11-09 02:58:54 +0100

    Now able to use generated BankingCore models in payment-service!
    Just need to actually implement them now. And generate the API in BankingCore as well..

--- payment-service/src/main/java/com/alphbank/payment/Application.java
+++ payment-service/src/main/java/com/alphbank/payment/Application.java
@@ -8,7 +8,7 @@ import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
+    static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
